d2f2e5352ab901d4f0057ac3a0d0a59d6cfe9160
eljardineronocturno <eljardineronocturno@eljardineronocturno.myguest.virtualbox.org>
2026-03-29 23:46:06 -0300

    js pruebas

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
 let firstName = "John";
 let lastName = "Doe";
+let a = 10;
+let b = 7 * a;
 
+const pi = 3.14;
+a = 2;
+
+
+console.log(23*2);
+console.log((2+7-5) / 51);
+console.log(a);
+console.log(b);
+console.log(pi);
 console.log(firstName);
 console.log(lastName);
+
